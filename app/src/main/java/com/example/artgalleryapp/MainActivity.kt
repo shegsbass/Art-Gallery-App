b13a69854b0113b1503ae90bc261c268fca7579f
Oladokun oluwasegun <oladokunoluwasegun@gmail.com>
2023-05-12 21:31:03 +0100

    Interacting next button

--- app/src/main/java/com/example/artgalleryapp/MainActivity.kt
+++ app/src/main/java/com/example/artgalleryapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.artgalleryapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.DrawableRes
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
@@ -18,16 +19,23 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ButtonElevation
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -54,6 +62,15 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ArtGalleryLayout(){
+    var nextArt: Int by remember { mutableStateOf(1) }
+
+    val artResource = when (nextArt){
+        1 -> R.drawable.img1
+        2 -> R.drawable.img2
+        3 -> R.drawable.img3
+        else -> R.drawable.img4
+    }
+
     Column (
         modifier = Modifier
             .fillMaxSize()
@@ -61,16 +78,68 @@ fun ArtGalleryLayout(){
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
     ) {
-        GalleryWall()
+        GalleryWall(
+            painter = artResource,
+            contentDescription = artResource.toString()
+        )
         GalleryDescription()
-        GalleryController()
+
+        Spacer(modifier = Modifier.height(40.dp))
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+
+            GalleryController(
+                onClick = { /*TODO*/ },
+                enabled = true,
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(0xFF3E001D),
+                    contentColor = Color.White
+                ),
+                elevation = ButtonDefaults.buttonElevation(
+                    defaultElevation = 8.dp,
+                    pressedElevation = 4.dp
+                ),
+                shape = RoundedCornerShape(16.dp),
+                border = BorderStroke(2.dp, Color(0xFF410002)),
+                contentPadding = PaddingValues(4.dp),
+                modifier = Modifier
+                    .width(100.dp)
+                    .height(50.dp),
+                text = R.string.prev
+            )
+
+            Spacer(modifier = Modifier.width(40.dp))
+
+            GalleryController(
+                onClick = { nextArt = (1..4).random()},
+                enabled = true,
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(0xFF3E001D),
+                    contentColor = Color.White
+                ),
+                elevation = ButtonDefaults.buttonElevation(
+                    defaultElevation = 8.dp,
+                    pressedElevation = 4.dp
+                ),
+                shape = RoundedCornerShape(16.dp),
+                border = BorderStroke(2.dp, Color(0xFF410002)),
+                contentPadding = PaddingValues(4.dp),
+                modifier = Modifier
+                    .width(100.dp)
+                    .height(50.dp),
+                text = R.string.next
+            )
+        }
+
+
     }
 
 
 }
 
 @Composable
-fun GalleryWall(modifier: Modifier = Modifier){
+fun GalleryWall(@DrawableRes painter: Int, contentDescription: String, modifier: Modifier = Modifier){
 
     Surface(
         modifier = Modifier
@@ -79,8 +148,10 @@ fun GalleryWall(modifier: Modifier = Modifier){
             .shadow(6.dp, RectangleShape)
             .padding(28.dp),
     ) {
-        Image(painter = painterResource(
-            id = R.drawable.img1), contentDescription = "First Image"
+        Image(
+            painter = painterResource(
+            id = painter),
+            contentDescription = contentDescription
         )
     }
 }
@@ -119,54 +190,24 @@ fun GalleryDescription(modifier: Modifier = Modifier){
 }
 
 @Composable
-fun GalleryController(){
-    Spacer(modifier = Modifier.height(40.dp))
-    Row(
-        horizontalArrangement = Arrangement.SpaceBetween
-    ) {
-        Button(onClick = { /*TODO*/ },
-            enabled = true,
-            colors = ButtonDefaults.buttonColors(
-                containerColor = Color(0xFF3E001D),
-                contentColor = Color.White
-            ),
-            elevation = ButtonDefaults.buttonElevation(
-                defaultElevation = 8.dp,
-                pressedElevation = 4.dp
-            ),
-            shape = RoundedCornerShape(16.dp),
-            border = BorderStroke(2.dp, Color(0xFF410002)),
-            contentPadding = PaddingValues(4.dp),
-            modifier = Modifier
-                .width(100.dp)
-                .height(50.dp)
-        ) {
-            Text(stringResource(R.string.prev) )
-        }
-        Spacer(modifier = Modifier.width(40.dp))
+fun GalleryController(onClick: () -> Unit, text: Int, enabled: Boolean = true, colors: ButtonColors, elevation: ButtonElevation,
+                      shape: RoundedCornerShape, border: BorderStroke,
+                      contentPadding: PaddingValues, modifier: Modifier) {
 
-        Button(onClick = { /*TODO*/ },
-            enabled = true,
-            colors = ButtonDefaults.buttonColors(
-                containerColor = Color(0xFF3E001D),
-                contentColor = Color.White
-            ),
-            elevation = ButtonDefaults.buttonElevation(
-                defaultElevation = 8.dp,
-                pressedElevation = 4.dp
-            ),
-            shape = RoundedCornerShape(16.dp),
-            border = BorderStroke(2.dp, Color(0xFF410002)),
-            contentPadding = PaddingValues(4.dp),
-            modifier = Modifier
-                .width(120.dp)
-                .height(50.dp)
-        ) {
-            Text(stringResource(R.string.next) )
-        }
-
-
+    Button(
+        onClick = onClick,
+        enabled = enabled,
+        colors = colors,
+        elevation = elevation,
+        shape = shape,
+        border = border,
+        contentPadding = contentPadding,
+        modifier = modifier
+    ){
+        Text(
+            text = stringResource(id = text))
     }
+
 }
 
 @Preview(showBackground = true)
