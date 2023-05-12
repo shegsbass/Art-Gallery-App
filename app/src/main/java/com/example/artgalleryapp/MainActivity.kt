c32300593867057ca151b86731fc7d80a091c8cc
Oladokun oluwasegun <oladokunoluwasegun@gmail.com>
2023-05-12 15:18:04 +0100

    Added GalleryDescription

--- app/src/main/java/com/example/artgalleryapp/MainActivity.kt
+++ app/src/main/java/com/example/artgalleryapp/MainActivity.kt
@@ -4,29 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.blur
 import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.graphics.Shadow
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.artgalleryapp.ui.theme.ArtGalleryAppTheme
@@ -58,6 +57,7 @@ fun ArtGalleryLayout(){
         verticalArrangement = Arrangement.Center,
     ) {
         GalleryWall()
+        GalleryDescription()
     }
 
 
@@ -70,12 +70,45 @@ fun GalleryWall(modifier: Modifier = Modifier){
         modifier = Modifier
             .fillMaxWidth()
             .size(400.dp)
-            .shadow(4.dp, RectangleShape)
+            .shadow(6.dp, RectangleShape)
             .padding(28.dp),
     ) {
         Image(painter = painterResource(
             id = R.drawable.img1), contentDescription = "First Image"
         )
+    }
+}
+
+@Composable
+fun GalleryDescription(modifier: Modifier = Modifier){
+    Spacer(modifier = Modifier.height(70.dp))
+
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .size(150.dp),
+        color = Color.LightGray
+    ){
+        Column(modifier = Modifier
+            .padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+        ) {
+            Text(text = stringResource(R.string.description_content),
+                style = MaterialTheme.typography.titleLarge, fontWeight = FontWeight.Light,
+                modifier = Modifier
+            )
+
+            Row() {
+                Text(text = stringResource(R.string.artist_name),
+                    style = MaterialTheme.typography.titleSmall,
+                    modifier = Modifier
+                )
+                Text(text = stringResource(R.string.art_year),
+                    style = MaterialTheme.typography.titleSmall, fontWeight = FontWeight.Light,
+                    modifier = Modifier
+                )
+            }
+        }
     }
 }
 
